032115ea90445f9b41e6dd573f87c51a51013e98
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2022-04-12 09:17:38 +0200

    Minor: gathered navigation code in one file

--- app/src/main/java/com/github/jetbrains/timetracker/androidapp/ui/TimeTrackerAppNavigation.kt
+++ app/src/main/java/com/github/jetbrains/timetracker/androidapp/ui/TimeTrackerAppNavigation.kt
@@ -1,14 +1,24 @@
 package com.github.jetbrains.timetracker.androidapp.ui
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.github.jetbrains.timetracker.androidapp.ui.home.HomeView
 import com.github.jetbrains.timetracker.androidapp.ui.slice.FinishedSliceView
 import com.github.jetbrains.timetracker.androidapp.ui.slice.RunningSliceView
+import com.github.jetbrains.timetracker.androidapp.ui.theme.TimeTrackerAppTheme
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import java.util.*
 
 enum class TimeTrackerScreen(val title: String) {
@@ -22,6 +32,44 @@ enum class TimeTrackerScreen(val title: String) {
                 ViewSlice.name -> ViewSlice
                 else -> throw IllegalArgumentException("Route $route is not recognized.")
             }
+    }
+}
+
+@Composable
+fun TimeTrackerApp() {
+    TimeTrackerAppTheme {
+        val navController = rememberNavController()
+        val backstackEntry = navController.currentBackStackEntryAsState()
+        val currentScreen = TimeTrackerScreen.fromRoute(
+            backstackEntry.value?.destination?.route
+        )
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    // TODO Show user details properly, add 'Log out'
+                    title = { Text(currentScreen.title + " (${Firebase.auth.currentUser?.displayName ?: ""})") },
+                    navigationIcon = if (currentScreen != TimeTrackerScreen.Home) {
+                        {
+                            IconButton(onClick = { navController.navigate(TimeTrackerScreen.Home.name) }) {
+                                Icon(
+                                    imageVector = Icons.Filled.ArrowBack,
+                                    contentDescription = "Back"
+                                )
+                            }
+                        }
+                    } else {
+                        null
+                    },
+                )
+            },
+        ) { innerPadding ->
+            Surface(
+                modifier = Modifier.padding(innerPadding),
+                color = MaterialTheme.colors.background
+            ) {
+                TimeTrackerNavHost(navController)
+            }
+        }
     }
 }
 
---
+++ app/src/main/java/com/github/jetbrains/timetracker/androidapp/ui/TimeTrackerAppWithAuthentication.kt
@@ -0,0 +1,42 @@
+package com.github.jetbrains.timetracker.androidapp.ui
+
+import androidx.compose.runtime.*
+import com.github.jetbrains.timetracker.androidapp.auth.AuthenticationProvider
+import com.github.jetbrains.timetracker.androidapp.ui.auth.LoginScreen
+import com.github.jetbrains.timetracker.androidapp.ui.theme.TimeTrackerAppTheme
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import org.koin.androidx.compose.get
+
+@Composable
+fun TimeTrackerAppWithAuthentication(
+    authProvider: AuthenticationProvider = get()
+) {
+    var showLoginScreen by remember { mutableStateOf(true) }
+    val auth: FirebaseAuth = Firebase.auth
+    if (auth.currentUser == null && showLoginScreen) {
+        var prevError by remember { mutableStateOf<String?>(null) }
+        TimeTrackerAppTheme {
+            LoginScreen(
+                onLogIn = { email: String, password: String ->
+                    authProvider.logIn(
+                        email, password,
+                        onSuccessfulLogIn = { showLoginScreen = false },
+                        onError = { exception -> prevError = exception?.message },
+                    )
+                },
+                onSignIn = { email: String, password: String, name: String ->
+                    authProvider.signUp(
+                        email, password, name,
+                        onSuccessfulSignIn = { showLoginScreen = false },
+                        onError = { exception -> prevError = exception?.message },
+                    )
+                },
+                previousErrorText = prevError,
+            )
+        }
+    } else {
+        TimeTrackerApp()
+    }
+}
